92f5097c4668245ab52d5dfdfbc144fe89a0fabd
Huyvictory <Huyvictory@HUYVICTORY-PC>
2024-03-13 23:56:30 +0700

    Add project files.

---
+++ IdentityServer/IdentityConfiguration.cs
@@ -0,0 +1,66 @@
+﻿// Ignore Spelling: Api
+
+using IdentityModel;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Security.Claims;
+
+namespace IdentityServer
+{
+    public class IdentityConfiguration
+    {
+        public static List<TestUser> TestUsers =>
+            new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "12345",
+                    Username = "Huy",
+                    Password = "@Password12345",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.Name, "Huy Vo"),
+                        new Claim(JwtClaimTypes.GivenName, "Huy Vo"),
+                        new Claim(JwtClaimTypes.FamilyName, "Vo"),
+                        new Claim(JwtClaimTypes.WebSite, "http://abcxyz.com"),
+                    }
+                }
+            };
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("myApi.read"),
+                new ApiScope("myApi.write"),
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new ApiResource[]
+            {
+                new ApiResource("myApi")
+                {
+                    Scopes = new List<string>{ "myApi.read","myApi.write" },
+                    ApiSecrets = new List<Secret>{ new Secret("supersecret".Sha256()) }
+                }
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "cwm.client",
+                    ClientName = "Client Credentials Client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedScopes = { "myApi.read" }
+                },
+            };
+    }
+}
